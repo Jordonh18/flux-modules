5320bf4ca78a850c9613d698e52e58acda4276dd
Jordon Harrison <Jordon.Harrison@outlook.com>
2026-02-05 14:07:34 +0000

    Add migration files for Databases module

---
+++ modules/Databases/migrations/001_initial.down.sql
@@ -0,0 +1,6 @@
+-- Rollback migration for Databases module
+-- Migration: 001_initial.down.sql
+--
+-- This file undoes the changes made in 001_initial.sql
+
+-- Placeholder rollback - no tables to drop yet
---
+++ modules/Databases/migrations/001_initial.sql
@@ -0,0 +1,8 @@
+-- Databases Module - Initial Schema
+-- Migration: 001_initial.sql
+--
+-- This file is automatically executed when the module is first loaded.
+-- Migrations are tracked in the module_migrations table.
+
+-- Placeholder migration - no tables created yet
+-- Database functionality will be added in future versions
